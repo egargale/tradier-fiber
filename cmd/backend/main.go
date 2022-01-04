1311bb89e1b5a6f6c65b9083986147f1c4c1e9d8
Enrico Gargale <enrico.gargale@gmail.com>
2022-01-04 10:10:44 +0100

    testing main

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,52 +1,172 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"tradier-fiber/internals/redis"
-	"tradier-fiber/internals/rest"
-	"tradier-fiber/internals/util"
-	// "github.com/gofiber/adaptor/v2"
-	// "github.com/davecgh/go-spew/spew"
+	"strconv"
+
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/middleware"
 )
 
+type Todo struct {
+	Id        int    `json:"id"`
+	Name      string `json:"name"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []*Todo{
+	{Id: 1, Name: "Walk the dog", Completed: false},
+	{Id: 2, Name: "Walk the cat", Completed: false},
+}
+
 func main() {
+	app := fiber.New()
 
-	// viper.AddConfigPath(".")
-	// viper.SetConfigName("app")
-	// viper.SetConfigType("env")
-	// viper.AutomaticEnv()
-	// err := viper.ReadInConfig()
-	// if err != nil {
-	// 	log.Fatalf("Error reading config file, %s", err)
-	// }
-	// err = viper.Unmarshal(&MyConfig)
-	// if err != nil {
-	// 	log.Fatalf("Unable to decode the config file, %v", err)
-	// }
-	// log.Printf("database uri is %s", MyConfig.TradierKey)
-	if err := util.LoadConfig("./"); err != nil {
-		panic(fmt.Errorf("invalid application configuration: %s", err))
+	app.Use(middleware.Logger())
+	app.Use(middleware.Recover())
+
+	app.Get("/", func(ctx *fiber.Ctx) {
+		ctx.Send("hello world")
+	})
+
+	SetupApiV1(app)
+
+	err := app.Listen(3300)
+	if err != nil {
+		panic(err)
 	}
-	// MyConfig, err := util.LoadConfig("./")
-	// if err != nil {
-	// 	log.Fatal("cannot load config:", err)
-	// }
-	log.Printf("Tradier Key: %s", util.MyConfig.TradierKey)
-	log.Printf("Tradier Account: %s", util.MyConfig.TradierAccount)
-	redis.TestRedis()
+}
 
-	// Get Postions
-	// rest.MyTradier_Market()
-	rest.MyTradierSandboxPosition()
-	rest.MyTradierProductionPosition()
+func SetupApiV1(app *fiber.App) {
+	v1 := app.Group("/v1")
 
-	// Setup endpoints
-	// app := fiber.New()
-	// // app.Get("/sse", adaptor.HTTPHandler(handler(dashboardHandler)))
-	// app.Get("/mm", tradier_market)
-	// app.Get("/market", func(c *fiber.Ctx) error {
-	// 	return c.SendString("Hello, World!") // send text
-	// })
-	// app.Listen(":3300")
+	SetupTodosRoutes(v1)
+}
+
+func SetupTodosRoutes(grp fiber.Router) {
+	todosRoutes := grp.Group("/todos")
+	todosRoutes.Get("/", GetTodos)
+	todosRoutes.Post("/", CreateTodo)
+	todosRoutes.Get("/:id", GetTodo)
+	todosRoutes.Delete("/:id", DeleteTodo)
+	todosRoutes.Patch("/:id", UpdateTodo)
+}
+
+func UpdateTodo(ctx *fiber.Ctx) {
+	type request struct {
+		Name      *string `json:"name"`
+		Completed *bool   `json:"completed"`
+	}
+
+	paramsId := ctx.Params("id")
+	id, err := strconv.Atoi(paramsId)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse id",
+		})
+		return
+	}
+
+	var body request
+	err = ctx.BodyParser(&body)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse body",
+		})
+		return
+	}
+
+	var todo *Todo
+
+	for _, t := range todos {
+		if t.Id == id {
+			todo = t
+			break
+		}
+	}
+
+	if todo == nil {
+		ctx.Status(fiber.StatusNotFound)
+		return
+	}
+
+	if body.Name != nil {
+		todo.Name = *body.Name
+	}
+
+	if body.Completed != nil {
+		todo.Completed = *body.Completed
+	}
+
+	ctx.Status(fiber.StatusOK).JSON(todo)
+}
+
+func DeleteTodo(ctx *fiber.Ctx) {
+	paramsId := ctx.Params("id")
+	id, err := strconv.Atoi(paramsId)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse id",
+		})
+		return
+	}
+
+	for i, todo := range todos {
+		if todo.Id == id {
+			todos = append(todos[0:i], todos[i+1:]...)
+			ctx.Status(fiber.StatusNoContent)
+			return
+		}
+	}
+
+	ctx.Status(fiber.StatusNotFound)
+}
+
+func GetTodo(ctx *fiber.Ctx) {
+	paramsId := ctx.Params("id")
+	id, err := strconv.Atoi(paramsId)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse id",
+		})
+		return
+	}
+
+	for _, todo := range todos {
+		if todo.Id == id {
+			ctx.Status(fiber.StatusOK).JSON(todo)
+			return
+		}
+	}
+
+	ctx.Status(fiber.StatusNotFound)
+}
+
+func CreateTodo(ctx *fiber.Ctx) {
+	type request struct {
+		Name string `json:"name"`
+	}
+
+	var body request
+
+	err := ctx.BodyParser(&body)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse json",
+		})
+		return
+	}
+
+	todo := &Todo{
+		Id:        len(todos) + 1,
+		Name:      body.Name,
+		Completed: false,
+	}
+
+	todos = append(todos, todo)
+
+	ctx.Status(fiber.StatusCreated).JSON(todo)
+}
+
+func GetTodos(ctx *fiber.Ctx) {
+	ctx.Status(fiber.StatusOK).JSON(todos)
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/goccy/go-json v0.8.1
+	github.com/gofiber/fiber v1.14.6
 	github.com/pkg/errors v0.9.1
 	github.com/valyala/fasthttp v1.31.0
 )
@@ -15,10 +16,14 @@ require (
 require (
 	github.com/andybalholm/brotli v1.0.2 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/klauspost/compress v1.13.4 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
@@ -30,6 +35,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
