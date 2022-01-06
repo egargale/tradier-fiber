97fbc5dbe522cea17f41d35c44c5ecefaad06d5d
Enrico Gargale <enrico.gargale@gmail.com>
2022-01-06 08:41:32 +0100

    Update repo.go

--- internals/postgresql/repo.go
+++ internals/postgresql/repo.go
@@ -1,23 +1,24 @@
 package postgresql
 
 import (
-	// "database/sql"
+	"database/sql"
 	// "context"
 	// "errors"
 
 	// "github.com/google/uuid"
 	// "github.com/jackc/pgx/v4"
 
-	//"github.com/egargale/tradier-fiber/internals"
+	// "github.com/egargale/tradier-fiber/internals"
 	"github.com/egargale/tradier-fiber/internals/postgresql/db"
 )
 
 type Repo struct {
 	q *db.Queries
+	DB *sql.DB
 }
 
-// func NewRepo(d db.DBTX) *Repo {
-// 	return &Repo{
-// 		q: db.New(d)
-// 	}
-// }
+func NewRepo(db *sql.DB) *Repo {
+	return &Repo{
+		DB: db,
+		q: db.New(db),}
+}
